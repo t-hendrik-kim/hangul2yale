ff041dd5d559659be4effe0a6e666b7478ccc3dc
Hendrik <9439810+t-hendrik-kim@users.noreply.github.com>
2023-02-05 16:16:10 -0800

    Update web.py

--- web.py
+++ web.py
@@ -1,22 +1,17 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import cgi
 import hangul2yale
 
-# メイン
+# Main
 if __name__ == "__main__":
-
-	print "Content-type: text/plain;charset=utf-8\n"
 
 	form = cgi.FieldStorage()
 	if form.getvalue('hangul'):
-
 		hangul = form.getvalue('hangul').decode('utf-8')
-		#dots = form.getvalue('dots')
-
 		result = hangul2yale.convert_string(string=hangul)
 	else:
-		result = ''
+		result = "No input provided. Please enter a Hangul string."
 	
-	print cgi.escape(result).encode('utf-8')
+	print("Content-type: text/plain;charset=utf-8\n")
+	print(cgi.escape(result).encode('utf-8'))
